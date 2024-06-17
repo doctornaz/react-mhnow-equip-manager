493d5e9b92cfe17da4c025be5d0c16345e3007c5
Jorge Samiento <jorge@finagraph.com>
2024-06-16 20:33:30 -0400

    somewhat working with skills list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import SkillSearch from './components/SkillSearch/SkillSearch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Skill Search</h1>
+        <SkillSearch />
       </header>
     </div>
   );
