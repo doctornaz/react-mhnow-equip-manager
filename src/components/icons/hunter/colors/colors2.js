a1fd34f13e35031270430c582736f87e5f45bc01
Naz <doctor.n4z@gmail.com>
2024-06-22 00:48:03 -0400

    added reusable components

---
+++ src/components/icons/hunter/colors/colors2.js
@@ -0,0 +1,64 @@
+const rankColors = {
+  1: {
+    fill1: "#6d6d6d",
+    fill2: "#888",
+    fill3: "#bebebe"
+  },
+  2: {
+    fill1: "#8f8f8f",
+    fill2: "#b5b5b5",
+    fill3: "#ededed"
+  },
+  3: {
+    fill1: "#667c16",
+    fill2: "#819e1f",
+    fill3: "#afd52e",
+  },
+  4: {
+    fill1: "#2b6f24",
+    fill2: "#378b2f",
+    fill3: "#4ebe43"
+  },
+  5: {
+    fill1: "#376e77",
+    fill2: "#498e98",
+    fill3: "#66c2cf"
+  },
+  6: {
+    fill1: "#353789",
+    fill2: "#484ab5",
+    fill3: "#6266f0"
+  },
+  7: {
+    fill1: "#583598",
+    fill2: "#7146c1",
+    fill3: "#9e63ff"
+  },
+  8: {
+    fill1: "#784026",
+    fill2: "#9d5433",
+    fill3: "#d37448"
+  },
+  9: {
+    fill1: "#6c243d",
+    fill2: "#8f3153",
+    fill3: "#bf4571"
+  },
+  10: {
+    fill1: "#018399",
+    fill2: "#02abc8",
+    fill3: "#03e5ff"
+  },
+  11: {
+    fill1: "#9c7c0a",
+    fill2: "#c9a111",
+    fill3: "#ffdc1b"
+  },
+  12: {
+    fill1: "#6f979d",
+    fill2: "#90c4cd",
+    fill3: "#c2ffff"
+  }
+};
+
+export default rankColors;
