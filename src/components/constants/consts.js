ee989ed088aea8764f5de506f1021615c0184fc7
Naz <doctor.n4z@gmail.com>
2024-07-21 15:47:30 -0400

    empty armor set

--- src/components/constants/consts.js
+++ src/components/constants/consts.js
@@ -1,1 +1,9 @@
 export const slotOrder = ['Head', 'Torso', 'Arms', 'Waist', 'Legs'];
+
+export const emptyArmorSet = {
+    'Head': "FREE SLOT",
+    'Torso': "FREE SLOT",
+    'Arms': "FREE SLOT",
+    'Waist': "FREE SLOT",
+    'Legs': "FREE SLOT"
+};
